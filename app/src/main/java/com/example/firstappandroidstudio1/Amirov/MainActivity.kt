21b6f7073a0710ac1a63b94b6d361049a31e91f4
Trustt52 <whytrttttbam@mail.ru>
2026-05-23 16:32:13 +0300

    gotoBo

---
+++ app/src/main/java/com/example/firstappandroidstudio1/Amirov/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.firstappandroidstudio.Amirov
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.firstappandroidstudio.Amirov.databinding.ActivityMainBinding
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonStart.setOnClickListener {
+            val randomNumber = Random.nextInt(1, 7)
+            binding.textViewDiceNum.text = randomNumber.toString()
+        }
+        binding.textViewConst.text = getString(R.string.program_ex)
+        binding.text.text = getString(R.string.text_text)
+        binding.buttonStart.text = getString(R.string.text_button_start)
+
+    }
+}
